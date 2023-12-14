63bb2bc1553d689c2775254136c812bc9549623f
Michael Donahue <81647737+mdonahue-godaddy@users.noreply.github.com>
2023-12-14 16:22:57 -0600

    Add example

--- cmd/go-qr-code-example/main.go
+++ cmd/go-qr-code-example/main.go
@@ -5,38 +5,44 @@ import (
 	"os"
 
 	"github.com/boombuler/barcode"
+	"github.com/boombuler/barcode/ean"
 	"github.com/boombuler/barcode/qr"
 )
 
 func main() {
 	// Create the barcode scaled to 256x256 pixels
-	err := createQR("https://www.linkedin.com/in/michael-donahue-15138314/", qr.M, qr.Auto, 256, 256, "linkedin-qrcode.png")
+	err := createQRCode("https://www.linkedin.com/in/michael-donahue-15138314/", qr.M, qr.Auto, 256, 256, "linkedin-qrcode.png")
 	if err != nil {
 		panic(err)
 	}
 
-	err = createQR("https://www.google.com/", qr.M, qr.Auto, 256, 256, "google-qrcode.png")
+	err = createQRCode("https://www.google.com/", qr.M, qr.Auto, 256, 256, "google-qrcode.png")
 	if err != nil {
 		panic(err)
 	}
 
-	err = createQR("https://www.godaddy.com/", qr.M, qr.Auto, 256, 256, "godaddy-qrcode.png")
+	err = createQRCode("https://www.godaddy.com/", qr.M, qr.Auto, 256, 256, "godaddy-qrcode.png")
+	if err != nil {
+		panic(err)
+	}
+
+	err = createEAN13("078693597244", 256, 128, "ean13-barcode.png")
 	if err != nil {
 		panic(err)
 	}
 }
 
-func createQR(content string, level qr.ErrorCorrectionLevel, mode qr.Encoding, width int, height int, fileName string) error {
+func createQRCode(content string, level qr.ErrorCorrectionLevel, mode qr.Encoding, width int, height int, fileName string) error {
 	// Create the barcode
-	qrCode, err := qr.Encode(content, level, mode)
+	code, err := qr.Encode(content, level, mode)
 	if err != nil {
 		return err
 	}
 
 	// Scale the barcode
-	qrCode, err = barcode.Scale(qrCode, width, height)
-	if err != nil {
-		return err
+	scale, serr := barcode.Scale(code, width, height)
+	if serr != nil {
+		return serr
 	}
 
 	// create the output file
@@ -47,7 +53,36 @@ func createQR(content string, level qr.ErrorCorrectionLevel, mode qr.Encoding, w
 	defer file.Close()
 
 	// encode the barcode as png
-	perr := png.Encode(file, qrCode)
+	perr := png.Encode(file, scale)
+	if perr != nil {
+		return perr
+	}
+
+	return nil
+}
+
+func createEAN13(content string, width int, height int, fileName string) error {
+	// Create the barcode
+	code, err := ean.Encode(content)
+	if err != nil {
+		return err
+	}
+
+	// Scale the barcode
+	scale, serr := barcode.Scale(code, width, height)
+	if serr != nil {
+		return serr
+	}
+
+	// create the output file
+	file, ferr := os.Create(fileName)
+	if ferr != nil {
+		return ferr
+	}
+	defer file.Close()
+
+	// encode the barcode as png
+	perr := png.Encode(file, scale)
 	if perr != nil {
 		return perr
 	}
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/mjdusa/go-qr-code-example
 
 go 1.21.5
 
-require github.com/boombuler/barcode v1.0.1 // indirect
+require github.com/boombuler/barcode v1.0.1
